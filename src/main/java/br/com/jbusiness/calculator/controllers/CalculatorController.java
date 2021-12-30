bb5ca68f56244ed6f65730fd50aa9570d9576994
rodrigoks <rodrigoks@gmail.com>
2021-12-30 11:21:22 -0300

    Refatoramento do codigo.

--- src/main/java/br/com/jbusiness/calculator/controllers/CalculatorController.java
+++ src/main/java/br/com/jbusiness/calculator/controllers/CalculatorController.java
@@ -1,4 +1,4 @@
-package br.com.jbusiness.calculator;
+package br.com.jbusiness.calculator.controllers;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
